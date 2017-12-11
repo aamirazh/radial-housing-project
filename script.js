e0de9cd8bebfd689d6af89fc1271d71f5c240387
sd268 <stephanie.ding@gmail.com>
2017-12-10 21:37:21 -0500

    adding api endpoint base code

---
+++ script.js
@@ -0,0 +1,10 @@
+function httpGet(apiUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", apiURl, true); 
+    xmlHttp.send(null);
+}
